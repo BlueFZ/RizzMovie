3adff49b74d18597542dd260094e96a20559034a
BlueFZ <muhammadsyafi@student.ub.ac.id>
2023-01-26 15:01:05 +0700

    1st commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import '../styles/home.css'
+import Arrow from '../../public/arrow.png'
+import Anim from '../../public/animate.png'
+import Movie from '../../public/movie.png'
+import Tv from '../../public/tv.png'
+import Trend from '../../public/trend.png'
+import { Link } from 'react-router-dom'
+
+function Homepage() {
+  return (
+   <>
+    <div className="judul-container">
+    <div className='judul'>
+      <h1>
+      Rizz Movies and TV Shows
+      </h1>
+      <p className='judul-txt'>
+        This website will privide you with the lists of Movies and TV Shows you've ever heard. So you'll be able to view a whole bunch of our lists. Feel free to explore to your heart content
+      </p>
+      <div className='btn-container'>
+        <a href="#list">
+          <button>
+            Our Lists
+            <img src={Arrow}/>
+          </button>
+        </a>
+      </div>
+    </div>
+    <div className='anim'>
+      <img src={Anim} className='pict' />
+    </div>
+    </div>
+    <div id='list' className="content">
+      <h2>Our Lists</h2>
+      <div className="card-container">
+        <div className="card">
+          <h3>Movies</h3>
+          <img src={Movie} alt="" />
+          <p className='card-desc'>
+            Here we Provide you with our movie lists based on our data base. Cli ck the button below to check out our movie lists.
+          </p>
+          <div className="btn-movie-container">
+            <Link to='/movie'>
+              <button className='btn-movie'>
+                <p className='btn-txt'>Movie Lists</p>
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <h3>TV Shows</h3>
+          <img src={Tv} alt="" />
+          <p className=''>
+            Here we Provide you with our tv show lists based on our data base. Click the button below to check out our tv show lists.
+          </p>
+          <div className="btn-movie-container">
+            <Link to='/movie'>
+              <button className='btn-movie'>
+                <p className='btn-txt'>TV Show Lists</p>
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <h3>Trending</h3>
+          <img src={Trend} alt="" />
+          <p className=''>
+            Here we Provide you with our trending shows based on our data base. Click the button below to check out our trending shows.
+          </p>
+          <div className="btn-movie-container">
+            <Link to='/movie'>
+              <button className='btn-movie'>
+                <p className='btn-txt'>Trending Shows</p>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+   </>
+  )
+}
+
+export default Homepage
---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,89 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/movielist.css'
+
+function MovieList() {
+  const [list, setList]= useState([])
+  const [page, setPage]= useState(1)
+  const [load, setLoad]= useState(true)
+
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/4/list/1?page=${page}&api_key=9e89dc1810e00461d6f59011e04175ed`).then((movies) => {
+      setList(movies.data.results)
+      setTimeout(() => setLoad(false), 1000)
+      console.log(movies.data.results)
+    })
+  }, [page])
+  return (
+   <>
+    {load ? (
+      <div className="container-svg">
+        <svg className='svg' viewBox='0 0 50 50'>
+          <circle
+            cx={25}
+            cy={25}
+            r={20}
+          />
+        </svg>
+      </div>
+    ) 
+    : 
+    (
+      <>
+        <div className="content">
+          <nav>
+            <ul>
+              <li>
+                <Link className='link' to='/'>
+                  Home
+                </Link>
+              </li>
+              <li
+              onClick={() => {
+                setPage(1)
+                console.log(page)
+              }}
+              >
+                Page 1              
+              </li>
+              <li
+              onClick={() => {
+                setPage(2)
+                console.log(page)
+              }}
+              >
+                Page 2
+              </li>
+              <li
+              onClick={() => {
+                setPage(3)
+                console.log(page)
+              }}
+              >
+                Page 3
+              </li>
+            </ul>
+          </nav>
+          <h1>Movie Lists</h1>
+          <div className="card-container">
+            {
+              list.map((movie) => {
+                return (
+                 <div className='card'>
+                  <h3 className='judul-card'>{movie.title}</h3>
+                  <img className='thumbnail' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+                  <p>{movie.overview}</p>
+                 </div>
+                )
+              })
+            }
+            </div>
+        </div>
+      </>
+    )}
+   </>
+  )
+}
+
+export default MovieList
